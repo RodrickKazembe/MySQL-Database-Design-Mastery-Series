4a58a6da5a5cadbbd5af49a52925781d52ab8114
Rodrick Kazembe <kazemberodrick@gmail.com>
2023-03-23 16:53:47 +0300

    Update 05_Simplify_Complex_Queries_and_Enhance_Your_Database_Experience.sql
    Update the script to answer all concepts in the video

--- 05_Simplify_Complex_Queries_and_Enhance_Your_Database_Experience.sql
+++ 05_Simplify_Complex_Queries_and_Enhance_Your_Database_Experience.sql
@@ -1,20 +1,61 @@
--- 1. Create a basic view displaying customer names and their total order amounts:
-CREATE VIEW customer_total_orders AS
-SELECT c.customer_id, c.first_name, c.last_name, SUM(o.order_value) AS total_order_amount
-FROM Customers c
-JOIN Orders o ON c.customer_id = o.customer_id
-GROUP BY c.customer_id;
+-- 1. Creating a simple view displaying customer names and total order amounts
+CREATE VIEW customer_orders_summary AS
+SELECT
+  c.customer_id,
+  c.first_name,
+  c.last_name,
+  SUM(o.order_value) AS total_order_amount
+FROM
+  Customers c
+  JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY
+  c.customer_id;
 
--- 2. Create a complex view using JOINs to display the details of each book and its author:
+
+-- 2. Creating a view displaying book details and their authors
 CREATE VIEW book_author_details AS
-SELECT b.book_id, b.title, a.author_id, a.first_name, a.last_name
-FROM Books b
-JOIN Authors a ON b.author_id = a.author_id;
-
--- 3. Create a complex view using a subquery to display the top-selling books and their total sales:
+SELECT
+  b.book_id,
+  b.title,
+  a.author_id,
+  a.first_name,
+  a.last_name
+FROM
+  Books b
+  JOIN Authors a ON b.author_id = a.author_id;
+  
+-- 3. Creating a view displaying top-selling books and their total sales
 CREATE VIEW top_selling_books AS
-SELECT b.book_id, b.title, SUM(o.quantity) AS total_sales
-FROM Books b
-JOIN order_items o ON b.book_id = o.book_id
-GROUP BY b.book_id
-ORDER BY total_sales DESC;
+SELECT
+  b.book_id,
+  b.title,
+  (
+    SELECT SUM(oi.quantity)
+    FROM Order_Items oi
+    WHERE oi.book_id = b.book_id
+  ) AS total_sales
+FROM
+  Books b
+ORDER BY
+  total_sales DESC;
+
+-- 4. Updating the top-selling books view to include the author name
+ALTER VIEW top_selling_books AS
+SELECT
+  b.book_id,
+  b.title,
+  a.first_name,
+  a.last_name,
+  (
+    SELECT SUM(oi.quantity)
+    FROM Order_Items oi
+    WHERE oi.book_id = b.book_id
+  ) AS total_sales
+FROM
+  Books b
+  JOIN Authors a ON b.author_id = a.author_id
+ORDER BY
+  total_sales DESC;
+
+-- 5. Dropping the view
+DROP VIEW top_selling_books;
