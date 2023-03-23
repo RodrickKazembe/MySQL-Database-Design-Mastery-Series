e684a96c093ea055842f039502f68d0437f62669
Rodrick Kazembe <kazemberodrick@gmail.com>
2023-03-23 16:04:48 +0300

    Update 04_Master_MySQL_Data_Retrieval_and_Manipulation.sql
    Update select update to filter for customers who live in a city that starts with the letter b

--- 04_Master_MySQL_Data_Retrieval_and_Manipulation.sql
+++ 04_Master_MySQL_Data_Retrieval_and_Manipulation.sql
@@ -1,19 +1,24 @@
 -- Fetching all data from the 'Customers' table
 SELECT * FROM Customers;
 
--- Fetching a list of customers who live in 'New York'
-SELECT * FROM Customers WHERE city = 'New York';
+-- Fetching a list of customers who live in a city that starts with 'B'
+SELECT customers.*,customer_addresses.city
+FROM customers 
+INNER JOIN customer_addresses ON customers.customer_id = customer_addresses.customer_id
+WHERE customer_addresses.city LIKE 'B%';
 
--- Sorting the list of New York customers by their last name in alphabetical order
-SELECT * FROM Customers WHERE city = 'New York' ORDER BY last_name ASC;
-Assuming you have an 'Orders' table with 'order_id', 'customer_id', 'order_date', and 'order_value' columns:
+-- Sorting the list of customers who live in a city that starts with b by their last name in alphabetical order
+SELECT customers.*,customer_addresses.city
+FROM customers 
+INNER JOIN customer_addresses ON customers.customer_id = customer_addresses.customer_id
+WHERE customer_addresses.city LIKE 'B%'
+ORDER BY last_name ASC;
 
--- Calculating the total revenue, average order value, and the highest and lowest order values
+-- Assuming you have an 'Orders' table with 'order_id', 'customer_id', 'order_date', and 'order_value' columns:
+---- Calculating the total revenue, average order value, and the highest and lowest order values
 SELECT
   SUM(order_value) AS total_revenue,
   AVG(order_value) AS average_order_value,
   MAX(order_value) AS highest_order_value,
   MIN(order_value) AS lowest_order_value
 FROM Orders;
-
--- These queries demonstrate the basic principles of data retrieval and manipulation in MySQL, as outlined in the video script. They show how to fetch data, filter results, sort data, and use aggregate functions to perform calculations on a group of rows.
